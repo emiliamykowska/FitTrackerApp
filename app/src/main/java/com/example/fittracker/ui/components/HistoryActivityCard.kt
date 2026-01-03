6e869dd1bcd95e8c4c49b844ee50250d1746e7e2
emilia <emilia.mykowska@gmail.com>
2026-01-03 15:36:41 +0100

    add HistoryActivityCard and add logic to HistoryScreen

---
+++ app/src/main/java/com/example/fittracker/ui/components/HistoryActivityCard.kt
@@ -0,0 +1,89 @@
+package com.example.fittracker.ui.components
+
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Composable
+fun HistoryActivityCard(
+    activityName: String,
+    duration: Int,
+    timestamp: Timestamp,
+    onDelete: () -> Unit
+){
+    val sdf = remember { SimpleDateFormat("MMM d, yyyy h:mm a", Locale.getDefault()) } //Locale.getDefault gives users country name of months fe
+    val formattedDate = sdf.format(timestamp.toDate()) //change timestamp to date and then format
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = RoundedCornerShape(15.dp),
+        border = BorderStroke(1.dp, Color.LightGray)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Column (
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ){
+                Text(
+                    text = activityName,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                )
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ){
+                    Text(
+                        text = "$duration min",
+                        fontSize = 13.sp
+                    )
+
+                    Text(
+                        text = formattedDate,
+                        fontSize = 13.sp
+                    )
+                }
+            }
+
+            IconButton (onClick = onDelete) {
+                Icon(
+                    imageVector = Icons.Default.DeleteForever,
+                    contentDescription = "Delete activity",
+                    tint = Color.Red
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/fittracker/ui/screens/HistoryScreen.kt
+++ app/src/main/java/com/example/fittracker/ui/screens/HistoryScreen.kt
@@ -1,9 +1,140 @@
 package com.example.fittracker.ui.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fittracker.data.ActivityEntry
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.example.fittracker.ui.components.HistoryActivityCard
+
 
 @Composable
 fun HistoryScreen(onNavigate: (String) -> Unit){
-    Text("History")
+    val db = FirebaseFirestore.getInstance()
+    val auth = Firebase.auth
+    val currentUser = auth.currentUser
+
+    var allActivities by remember { mutableStateOf<List<ActivityEntry>>(emptyList()) }
+
+    var showDeleteAlert by remember { mutableStateOf(false) }
+    var activityToDelete by remember { mutableStateOf<ActivityEntry?>(null) }
+    val context = LocalContext.current
+
+    LaunchedEffect(currentUser) {
+        if (currentUser != null) {
+            db.collection("activities")
+                .whereEqualTo("userId", currentUser.uid) //take activities with field userId = currentUSer.uid
+                .orderBy("date", Query.Direction.DESCENDING)
+                .addSnapshotListener { snapshot, error -> // listen for changes in firebase
+                    if (error != null) {
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null){ //if is not null that means that collection("activities") was changed f.e. new activity added
+                        allActivities = snapshot.toObjects(ActivityEntry::class.java)} //to objects change json from firestore into ActivityEntry
+                    //Use ActivityEntry class but since firebase is in java, it's kotlin structure has to be changes into class.java
+                }
+        }
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        item {
+            Text(
+                text = "All Activities:",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        items(items = allActivities){ activity ->
+            HistoryActivityCard(
+                activityName = activity.activityName,
+                duration = activity.duration,
+                timestamp = activity.date,
+                onDelete = {
+                    activityToDelete = activity
+                    showDeleteAlert = true
+                }
+            )
+        }
+
+
+        if (allActivities.isEmpty()) {
+            item {
+                Text(
+                    text = "You haven't add any activities yet, add your first one after a workout!",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 15.sp
+                )
+            }
+        }
+    }
+
+
+    if (activityToDelete != null && showDeleteAlert){
+        AlertDialog(
+            onDismissRequest = { showDeleteAlert = false },
+            title = {Text(text = "Delete activity")},
+            text = {Text(text = "Are you sure you want to delete this activity?")},
+            confirmButton = {TextButton(onClick = {
+                db.collection("activities")
+                    .document(activityToDelete!!.id)
+                    .delete() //document gets an entry from collection with specified path (here activity.id)
+                    .addOnSuccessListener { Toast.makeText(context, "Activity was successfully deleted", Toast.LENGTH_SHORT).show() }
+                    .addOnFailureListener { exception ->
+                    Toast.makeText(context, exception.localizedMessage, Toast.LENGTH_LONG).show()}
+
+                showDeleteAlert = false
+                    }
+            ) {
+                    Text(
+                        text = "Confirm",
+                        color = Color.Red
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        showDeleteAlert = false
+                    }
+                ) {
+                    Text(text = "Dismiss")
+                }
+            }
+        )
+
+    }
 }
